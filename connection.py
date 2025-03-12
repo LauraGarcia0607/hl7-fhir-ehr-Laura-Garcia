9b0b1af134e474109dd67dbf64f9d69a4ad116c2
LauraGarcia0607 <lau.garcia0723@gmail.com>
2025-03-12 17:05:31 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://mardugo:$Hola123.@sampleinformationservic.t2yog.mongodb.net/?retryWrites=true&w=majority&appName=SampleInformationService"
+    uri = "mongodb+srv://laugarcia0723:Laura0723ga@clusterpatient.aa4sy.mongodb.net/?retryWrites=true&w=majority&appName=ClusterPatient"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
